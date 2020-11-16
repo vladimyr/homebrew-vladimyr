671ce89121048b528c2af0c4dfeeb962f6aff0b1
Dario Vladovic <d.vladimyr@gmail.com>
2020-11-16 20:29:43 +0100

    Update pbzx

--- Formula/pbzx.rb
+++ Formula/pbzx.rb
@@ -1,13 +1,11 @@
 class Pbzx < Formula
-  desc "The pbzx stream parser"
+  desc "Unpack .pkg files"
   homepage "https://github.com/NiklasRosenstein/pbzx/"
   url "https://github.com/NiklasRosenstein/pbzx/archive/v1.0.2.tar.gz"
   sha256 "33db3cf9dc70ae704e1bbfba52c984f4c6dbfd0cc4449fa16408910e22b4fd90"
   head "https://github.com/NiklasRosenstein/pbzx.git"
 
   depends_on "xz"
-
-  uses_from_macos "libxar"
 
   def install
     system ENV.cc, "-llzma", "-lxar", "pbzx.c", "-o", "pbzx"
