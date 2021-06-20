ec793f01bb9d906eaba369221f01d0abb5fd2b7e
Dario Vladovic <d.vladimyr@gmail.com>
2021-06-20 04:27:16 +0200

    unrar: update from 6.0.3 to 6.0.7

--- Formula/unrar.rb
+++ Formula/unrar.rb
@@ -1,8 +1,13 @@
 class Unrar < Formula
   desc "Extract, view, and test RAR archives"
   homepage "https://www.rarlab.com/"
-  url "https://www.rarlab.com/rar/unrarsrc-6.0.3.tar.gz"
-  sha256 "1def53392d879f9e304aa6eac1339cf41f9bce1111a2f5845071665738c4aca0"
+  url "https://www.rarlab.com/rar/unrarsrc-6.0.7.tar.gz"
+  sha256 "a7029942006cbcced3f3b7322ec197683f8e7be408972ca08099b196c038f518"
+
+  livecheck do
+    url "https://www.rarlab.com/rar_add.htm"
+    regex(/href=.*?unrarsrc[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   def install
     # upstream doesn't particularly care about their unix targets,
