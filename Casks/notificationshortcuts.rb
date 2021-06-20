750ffbaa250e9c43e75bd1c601ea50e5c349b01f
Dario Vladovic <d.vladimyr@gmail.com>
2021-06-20 03:25:56 +0200

    notificationshortcuts: add livecheck

--- Casks/notificationshortcuts.rb
+++ Casks/notificationshortcuts.rb
@@ -2,11 +2,15 @@ cask "notificationshortcuts" do
   version "1.0.1"
   sha256 "181bd9e5b2e83349a030282564639d4e84ff52a12a510d1f82e8331b981f9b00"
 
-  # delpriore-appcasts.s3-us-west-1.amazonaws.com/NotificationShortcuts was verified as official when first introduced to the cask
-  url "https://delpriore-appcasts.s3-us-west-1.amazonaws.com/NotificationShortcuts-#{version}.zip"
-  appcast "https://delpriore-appcasts.s3-us-west-1.amazonaws.com/NotificationShortcutsCast.xml"
+  url "https://delpriore-appcasts.s3-us-west-1.amazonaws.com/NotificationShortcuts-#{version}.zip",
+      verified: "delpriore-appcasts.s3-us-west-1.amazonaws.com/"
   name "NotificationShortcuts"
   homepage "https://notificationshortcuts.com/"
+
+  livecheck do
+    url "https://delpriore-appcasts.s3-us-west-1.amazonaws.com/NotificationShortcutsCast.xml"
+    strategy :sparkle, &:short_version
+  end
 
   auto_updates true
   depends_on macos: ">= :sierra"
