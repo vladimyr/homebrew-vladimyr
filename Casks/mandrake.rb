7204321112945fe78c537461c60b63a650a790cd
Dario Vladovic <d.vladimyr@gmail.com>
2021-06-20 03:13:11 +0200

    mandrake: add livecheck

--- Casks/mandrake.rb
+++ Casks/mandrake.rb
@@ -3,9 +3,14 @@ cask "mandrake" do
   sha256 "55d137f32c5f34200b05e26b601963f2bcefc0332a4c80e62af6131bdd3cb4c7"
 
   url "https://sveinbjorn.org/files/software/mandrake/ManDrake-#{version}.zip"
-  appcast "https://sveinbjorn.org/files/appcasts/ManDrakeAppcast.xml"
   name "ManDrake"
+  desc "Man page editor"
   homepage "https://sveinbjorn.org/mandrake"
+
+  livecheck do
+    url "https://sveinbjorn.org/files/appcasts/ManDrakeAppcast.xml"
+    strategy :sparkle, &:short_version
+  end
 
   auto_updates true
 
