22397964e4634788f1655786d4c01606a138d8b4
Dario Vladovic <d.vladimyr@gmail.com>
2021-06-20 04:11:58 +0200

    hopper-disassembler: update from 4.5.22 to 4.7.7
    Add livecheck.

--- Casks/hopper-disassembler.rb
+++ Casks/hopper-disassembler.rb
@@ -1,12 +1,16 @@
 cask "hopper-disassembler" do
-  version "4.5.22-demo"
-  sha256 "6b99f7d185fa6e09147fad4ab1ef8a8567ebf89ed2c598164a022ceb85f3450b"
+  version "4.7.7-demo"
+  sha256 "b80789775f4151c9149790f96a09c20e4853b20cd8dcb5a91ba5037f80b56e70"
 
-  # d2ap6ypl1xbe4k.cloudfront.net was verified as official when first introduced to the cask
-  url "https://d2ap6ypl1xbe4k.cloudfront.net/Hopper-#{version}.dmg"
-  appcast "https://www.hopperapp.com/rss/html_changelog.php"
+  url "https://d2ap6ypl1xbe4k.cloudfront.net/Hopper-#{version}.dmg",
+      verified: "d2ap6ypl1xbe4k.cloudfront.net/"
   name "Hopper Disassembler"
   homepage "https://www.hopperapp.com/"
+
+  livecheck do
+    url "https://www.hopperapp.com/HopperWeb/appcast_v#{version.major}.php?Demo=true"
+    strategy :sparkle
+  end
 
   auto_updates true
 
